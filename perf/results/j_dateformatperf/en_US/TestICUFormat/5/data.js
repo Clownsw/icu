bb8266ed703e720ffe41f9c30d81033535368d86
github-action-benchmark <github@users.noreply.github.com>
2023-02-11 19:45:02 +0000

    add Benchmark (ndjson) benchmark result for b20c97009c29a6c230b834a02dea93bc7c2c1e7b

--- perf/results/j_dateformatperf/en_US/TestICUFormat/5/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/5/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1676142484829,
+  "lastUpdate": 1676144702178,
   "repoUrl": "https://github.com/Clownsw/icu",
   "entries": {
     "Benchmark": [
@@ -2728,6 +2728,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 332.53091907205976,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "b20c97009c29a6c230b834a02dea93bc7c2c1e7b",
+          "message": "ICU-22194 Add User Guide doc for MessageFormat 2.0 tech preview impl\n\nSee #2313",
+          "timestamp": "2023-02-10T21:17:59-05:00",
+          "tree_id": "beea462ce3cb4c5c6e8d5e465956091c0c65bedc",
+          "url": "https://github.com/Clownsw/icu/commit/b20c97009c29a6c230b834a02dea93bc7c2c1e7b"
+        },
+        "date": 1676144693917,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 372.2713619556957,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
