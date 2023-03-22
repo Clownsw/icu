8ce7f1adb8a93d6656e7a197c84d23fea358d3f3
github-action-benchmark <github@users.noreply.github.com>
2023-03-22 06:15:06 +0000

    add Benchmark (ndjson) benchmark result for 49e8b992962c048a31bc542d915a9daa6f4b3dc5

--- perf/results/j_dateformatperf/en_US/TestICUFormat/4/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/4/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679236161017,
+  "lastUpdate": 1679465706373,
   "repoUrl": "https://github.com/Clownsw/icu",
   "entries": {
     "Benchmark": [
@@ -2548,6 +2548,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 283.3670568122632,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yumaoka@users.noreply.github.com",
+            "name": "Yoshito Umaoka",
+            "username": "yumaoka"
+          },
+          "committer": {
+            "email": "yumaoka@users.noreply.github.com",
+            "name": "Yoshito Umaoka",
+            "username": "yumaoka"
+          },
+          "distinct": true,
+          "id": "49e8b992962c048a31bc542d915a9daa6f4b3dc5",
+          "message": "ICU-22220 BRS73 Clean up Java import statements",
+          "timestamp": "2023-03-21T15:35:52-04:00",
+          "tree_id": "af2d0660599b43060cbd604b64fa84e6e65323bf",
+          "url": "https://github.com/Clownsw/icu/commit/49e8b992962c048a31bc542d915a9daa6f4b3dc5"
+        },
+        "date": 1679465696629,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 353.02043825316196,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
